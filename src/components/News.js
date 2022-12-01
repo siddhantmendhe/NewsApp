a5627191cb28313ef889e8e64283f6ca5a49b2aa
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2022-12-01 18:45:11 +0530

    added loading animation

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
     
@@ -13,11 +14,15 @@ export class News extends Component {
         }
     }
    async componentDidMount(){
-      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=1&PageSize=20"
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=1&PageSize=${this.props.pageSize}`
+      this.setState({loading:true});
       let data= await fetch(url);
       let parsedData= await data.json();
       this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults});
       console.log(parsedData);
+      this.setState({
+        loading:false
+    });
 
        }
 
@@ -26,38 +31,43 @@ export class News extends Component {
 
         }else{
        
-        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=${this.state.page-1}&PageSize=20`;
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=${this.state.page-1}&PageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
         let data= await fetch(url);
         let parsedData= await data.json();
         this.setState({articles:parsedData.articles});
         this.setState({
-            page:this.state.page-1
+            page:this.state.page-1,
+            loading:false
         });
         }
        
 
        }
        handleNextClick= async()=>{
-        if(this.state.page+1> Math.ceil(this.state.totalResults/20)){
-
-        }else{
-            let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=${this.state.page+1}&PageSize=20`;
+        
+            let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=${this.state.page+1}&PageSize=${this.props.pageSize}`;
+            this.setState({loading:true});
             let data= await fetch(url);
             let parsedData= await data.json();
             this.setState({articles:parsedData.articles});
             this.setState({
-                page:this.state.page+1
+                page:this.state.page+1,
+                loading:false
             });
-        }
+        
+           
+        
 
        }
 
     render() {
         return (
           <div className="container my-3">
-                <h2>NewsMonkey - Top Headlines</h2>
+                <h1 className='text-center'>NewsApp - Top Headlines</h1>
+                {this.state.loading && <Spinner/>}
                 <div className="row"> 
-               { this.state.articles.map((element)=>{
+               {!this.state.loading && this.state.articles.map((element)=>{
                     return  <div className="col-md-4" key={element.url}>
                     <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage?element.urlToImage:"https://via.placeholder.com/300.png/09f/fff%20C/O%20https://placeholder.com/"} newsUrl={element.url}/>
                 </div>
@@ -68,7 +78,7 @@ export class News extends Component {
 
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&laquo; Previous</button>
-                    <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &raquo;</button>
+                    <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &raquo;</button>
 
 
                 </div>
