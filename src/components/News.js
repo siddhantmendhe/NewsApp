811b3b4c249658f87d8190b396790b58dd3d6e73
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2022-12-04 20:10:53 +0530

    title update

--- src/components/News.js
+++ src/components/News.js
@@ -16,15 +16,19 @@ export class News extends Component {
         category:PropTypes.string
 
     }
+    capitalizeFirstLetter=(string)=> {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
     
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
 
         this.state = {
             articles:[],
             loading: false,
             page:1
         }
+        document.title=`${this.capitalizeFirstLetter(this.props.category)} - NewsApp`;
     }
    async componentDidMount(){
       let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9fc68d4b53eb4bdca86447df88a0e505&page=1&PageSize=${this.props.pageSize}`
@@ -77,7 +81,7 @@ export class News extends Component {
     render() {
         return (
           <div className="container my-3">
-                <h1 className='text-center'>NewsApp - Top Headlines</h1>
+                <h1 className='text-center'>NewsApp - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner/>}
                 <div className="row"> 
                {!this.state.loading && this.state.articles.map((element)=>{
